07482573e874c02af3f45a62c8b68d9dad88223c
bembenk18 <putrabayu0180@gmail.com>
2025-05-04 11:22:30 +0700

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    
+
+    public function run(): void
+{
+    User::create([
+        'name' => 'Admin',
+        'email' => 'admin@example.com',
+        'password' => Hash::make('password'),
+    ]);
+}
+}
